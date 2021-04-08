0ea4d8fc739449f13e22a12aada007c9808e2e48
Camilo Arango <camiloarango@seven4n.com>
2021-04-08 15:43:31 -0500

    Changed http request to https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
   )}&limit=10&api_key=jZS1G9IOVoOWvEFZMEP3pdcSB7ti75xT`;
   const resp = await fetch(url);
