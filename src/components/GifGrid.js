cb56a066a6262b287a98def3c7eb4d080d9edc92
Camilo Arango <camiloarango@seven4n.com>
2021-04-08 15:35:23 -0500

    App finished and ready to be deployed

---
+++ src/components/GifGrid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({ category }) => {
+  // const [images, setImages] = useState([]);
+
+  // useEffect(() => {
+  //   getGifs(category).then(setImages);
+  // }, [category]);
+
+  const { data: images, loading } = useFetchGifs(category);
+
+  if (loading)
+    return <p className='animate__animated animate__flash'>Loading...</p>;
+  return (
+    <>
+      <h3> {category} </h3>
+      <div className='card-grid'>
+        {images.length > 0 &&
+          images.map((img) => <GifGridItem key={img.id} {...img} />)}
+      </div>
+    </>
+  );
+};
